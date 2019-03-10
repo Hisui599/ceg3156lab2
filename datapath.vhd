479f1b9419d294718ca038cbe18c75f0eaf6605e
cory maklin <corymaklin@gmail.com>
2019-03-10 18:43:27 -0400

    added shiftleft

--- datapath.vhd
+++ datapath.vhd
@@ -19,6 +19,7 @@ ARCHITECTURE RTL OF Datapath IS
 	Signal branchresult : std_logic_vector(7 downto 0);
 	Signal readdata1, readdata2, writedata, selectB, selectPC : std_logic_vector(7 DOWNTO 0);
 	Signal PCtemp, PC, newPC : std_logic_vector(7 DOWNTO 0);
+	signal IRSHL2 : std_logic_vector(7 downto 0);
 	Signal IR : std_logic_vector(31 DOWNTO 0);
 	Signal ZEROsignal: std_logic;
 	signal writeregout : std_logic_vector(4 downto 0);
@@ -39,7 +40,13 @@ ARCHITECTURE RTL OF Datapath IS
 --	);
 --END component;	
 
-component instructionmemory
+--component instructionmemory
+--port(
+--	address : in std_logic_vector(7 downto 0);
+--	instruction : out std_logic_vector(31 downto 0));
+--end component;
+
+component rom is
 port(
 	address : in std_logic_vector(7 downto 0);
 	instruction : out std_logic_vector(31 downto 0));
@@ -120,14 +127,6 @@ port (
 		zero : out std_logic);
 end component;
 
-component shiftLeft8bit
-PORT ( 	
-	X:IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-	clk, reset, ld: In STD_LOGIC;
-	Y:OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
-	
-end component;
-
 component mux3x8
 	port (
 		a, b, c, d, e, f: in std_logic_vector(7 downto 0);
@@ -149,6 +148,12 @@ component eightbitregister
 		o_Value			: OUT	STD_LOGIC_VECTOR(7 downto 0));
 END component;
 
+component shiftleft2 is
+port(
+	x : in std_logic_vector(7 downto 0);
+	y : out std_logic_vector(7 downto 0));
+end component;
+
 BEGIN
 
 --PC <= "00000000";
@@ -157,7 +162,7 @@ ProgramCounter: eightbitregister port map (greset, '1', gclock, newPC, PC);
 
 PCplus4: fullAdder8Bit port map(PC, "00000100",'0', open, PCtemp);
 
-INSTRUCTION: instructionmemory port map(PC,IR);
+INSTRUCTION: rom port map(PC,IR);
 
 CONTROL: controlunit port map(IR(31 downto 26), RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, Jump, ALUOp(1), ALUOp(0));
 
@@ -165,7 +170,7 @@ WRITEreg: mux21_5bit port map(IR(20 downto 16), IR(15 downto 11), RegDst, writer
 
 REGISTERS: registerfile port map(gclock, greset, IR(25 downto 21),IR(20 downto 16), writeregout, writedata, RegWrite, readdata1, readdata2);
 
-SELECTread2: mux21_8bit port map(readdata2, IR(7 downto 0),ALUSrc,selectB);
+SELECTread2: mux21_8bit port map(readdata2, IR(7 downto 0), ALUSrc, selectB);
 
 ALUCON: ALUcontrol port map(IR(5 downto 0), ALUOp, ALUOPERATION);
 
@@ -175,15 +180,15 @@ DATAMEM: ram port map(ALUresult, gclock, readdata2, MemWrite, DMEMresult); -- Me
 
 SelECTwrite: mux21_8bit port map (ALUresult, DMEMresult, MEMtoReg, writedata);
 
---REGISTERS2: registerfile port map(gclock,greset,IR(25 downto 21),IR(20 downto 16), writeregout, writedata, RegWrite, readdata1, readdata2);
-
-ADDNEXTPC: fullAdder8Bit port map(PCtemp, IR(7 downto 0), c, open, branchresult);
+ADDNEXTPC: fullAdder8Bit port map(PCtemp, IR(7 downto 0), '0', open, branchresult);
 
 s0 <= ZEROsignal AND Branch;
 
 BRANCHSELECT: mux21_8bit port map(PCtemp, branchresult, s0, selectPC);
 
-JUMPSELECT: mux21_8bit port map(branchresult, IR(7 downto 0), Jump, newPC);
+SL2 : shiftleft2 port map (IR(7 downto 0), IRSHL2);
+
+JUMPSELECT: mux21_8bit port map(selectPC, IRSHL2, Jump, newPC);
 
 flags <= '0' & REgDst & Jump & MemRead & MemtoReg & AluOp & alusrc;
 
